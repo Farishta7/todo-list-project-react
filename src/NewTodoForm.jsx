41ba7e8dea2365c1adaf7b1a17a586d8838db495
Farishta <farishtatq@gmail.com>
2023-09-09 12:46:20 +0100

    changed inut text field color

--- src/NewTodoForm.jsx
+++ src/NewTodoForm.jsx
@@ -1,31 +1,34 @@
 import { useState } from "react";
 
-export function NewTodoForm({onSubmit}) {
+export function NewTodoForm({ onSubmit }) {
   const [newItem, setNewItem] = useState("");
 
   function handleSubmit(event) {
-      event.preventDefault();
+    event.preventDefault();
     //   The following is saying if the input field is empty and "Add" has been clicked, we don't want it to render anything and the "return" helps to achieve this
-      if (newItem === "") return
+    if (newItem === "") return;
 
-      onSubmit(newItem);
+    onSubmit(newItem);
 
     setNewItem("");
   }
   return (
     <form onSubmit={handleSubmit} className="new-item-form">
       <div className="form-row">
-              <label htmlFor="item" style={{fontSize: '30px'}}>Time for the daily to do list!</label>
-              <br></br>
+        <label htmlFor="item" style={{ fontSize: "30px" }}>
+          Time for the daily to do list!
+        </label>
+        <br></br>
         <input
           value={newItem}
           onChange={(event) => setNewItem(event.target.value)}
           type="text"
-                  id="item"
-                  placeholder="Write here ..."
+          id="item"
+          placeholder="Write here ..."
+          className="form-input-box"
         />
-          </div>
-          
+      </div>
+
       <button className="btn">Add item</button>
     </form>
   );
